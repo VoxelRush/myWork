cc95f0769c25de556eaed3b0c8a52cd8827f4605
Vedant Thakkar <vedantyat@gmail.com>
2018-09-26 09:08:48 -0500

    Voting Program

--- src/mathOperators/Vote.java
+++ src/mathOperators/Vote.java
@@ -2,17 +2,19 @@ package mathOperators;
 
 import java.util.Scanner;
 
-public class mathOperators {
+public class Vote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int age = 1;
+		int year = 2018;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Your age?");
 		age = input.nextInt();
-		age = age + 7;
-		System.out.println("Your age in 7 years is " + age);
+		age = 18 - age;
+		year = age + year;
+		System.out.println("You can vote in " + year + " in " + age + " years");
 	}
 
 }

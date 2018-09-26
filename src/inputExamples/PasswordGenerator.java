991d3d795ed947416daeb68a8baef25ef7eea359
Vedant Thakkar <vedantyat@gmail.com>
2018-09-26 08:59:38 -0500

    Age Program

--- src/inputExamples/PasswordGenerator.java
+++ src/inputExamples/PasswordGenerator.java
@@ -18,6 +18,12 @@ public class PasswordGenerator {
 		System.out.println("Number 3?");
 		number3 = input.nextInt();
 		System.out.println(number1 +""+number2+""+number3);
+		System.out.println(number2 +""+number1+""+number3);
+		System.out.println(number3 +""+number1+""+number2);
+		System.out.println(number1 +""+number3+""+number2);
+		System.out.println(number1 +""+number2+""+number3);
+		System.out.println(number1 +""+number2+""+number3);
+		
 		
 	}
 

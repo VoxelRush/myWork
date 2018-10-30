cc5062c8042a1d755f6755302c92b62310473070
Vedant Thakkar <vedantyat@gmail.com>
2018-10-30 08:03:23 -0500

    Unfinished Decision Structures

---
+++ src/decisionStructures/GPA.java
@@ -0,0 +1,24 @@
+package decisionStructures;
+
+import java.util.Scanner;
+
+public class GPA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double GPA = 1;
+		double grade= 1;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Gpa and which year in highschool?");
+		GPA = input.nextDouble();
+		grade = input.nextDouble();
+		
+			if (GPA == 4.0 || grade == 4);
+				System.out.println("You work!");
+			else
+				System.out.println("OOF");
+				
+		
+	
+
+}

60f18a8e0382e82b6d3976c3bf767fe3879cfddb
Vedant Thakkar <vedantyat@gmail.com>
2018-11-26 09:04:08 -0600

    Test for New Branch

--- src/randomNumbers/RollADieHW.java
+++ src/randomNumbers/RollADieHW.java
@@ -1,10 +1,13 @@
 package randomNumbers;
-
+import java.util.Scanner;
 public class RollADieHW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		int min = 1; 
+		int max = input.nextInt();
 	}
 
 }

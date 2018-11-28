4b765550c7890daf12707b3f5adbe548362672d9
Vedant Thakkar <vedantyat@gmail.com>
2018-11-27 22:05:25 -0600

    Some String Programs

--- src/randomNumbers/RollADieHW.java
+++ src/randomNumbers/RollADieHW.java
@@ -1,13 +1,22 @@
 package randomNumbers;
+import java.util.Random;
 import java.util.Scanner;
 public class RollADieHW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
-		int min = 1; 
+		System.out.println("What range do u want for you dice");
+		int min = 1;
 		int max = input.nextInt();
-	}
+		int randomNum = 0;
+		System.out.println("What amount of rolls?");
+		int x = input.nextInt();
+		Random generator = new Random(5);
+		for (int count = 1; count<=x; count++) {
+			randomNum = min + generator.nextInt(max-min+1);
+			System.out.println(randomNum);
+		}
+	} 
 
 }
---
+++ src/strings/AgeFullName.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.Scanner;
+
+public class AgeFullName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		System.out.println("What is the age?");
+		int age = input.nextInt();
+		System.out.println("what is your full name?");
+		String words = input.nextLine();
+	}
+
+}

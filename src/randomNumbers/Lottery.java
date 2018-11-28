2e7c9517ff08e52117fe22bb368e60273114f9c3
VedantThakkar <vedantyat@gmail.com>
2018-11-27 23:41:05 -0600

    bro what r these deletes tho

---
+++ src/randomNumbers/Lottery.java
@@ -0,0 +1,13 @@
+package randomNumbers;
+
+import java.util.Scanner;
+
+public class Lottery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Write ")
+	}
+
+}

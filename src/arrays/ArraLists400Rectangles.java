fec29673046e83a73a0a2e8c4e5e7c241f58e6fe
Vedant Thakkar <vedantyat@gmail.com>
2019-01-08 08:18:58 -0600

    ArrayList1

--- src/arrays/ArraLists400Rectangles.java
+++ src/arrays/ArraLists400Rectangles.java
@@ -1,10 +1,23 @@
 package arrays;
 
+import java.util.ArrayList;
+
+import objectOrientedProgramming.Rectangle;
+
 public class ArraLists400Rectangles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Rectangle> List = new ArrayList<Rectangle>();
+		int min = 50;
+		int max = 70;
+		for (int x = 0; x < 400;x++) {
+			int j =  min + (int)(Math.random()*(max-min+1));
+			int w =  min + (int)(Math.random()*(max-min+1));
+			List.add(new Rectangle(j,w));
+			System.out.println(List.get(x).area());
+		}
+		
 	}
 
 }

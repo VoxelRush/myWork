6096d66d2ac8cbd64aa1a16a7fa24850547be5fd
Vedant Thakkar <vedantyat@gmail.com>
2019-02-14 20:03:27 -0600

    Crap

--- src/afterUnits/HardDrive.java
+++ src/afterUnits/HardDrive.java
@@ -1,5 +1,34 @@
 package afterUnits;
 
-public class HardDrive {
+import java.util.ArrayList;
 
+public class HardDrive {
+	
+	private String name;
+	private double capacity;
+	private double freespace;
+	private ArrayList <Double> file;
+	
+	public HardDrive(String xname, double xcapacity, double xfreespace) {
+		name = xname;
+		capacity = xcapacity;
+		freespace = xfreespace;
+	}
+	public void addfile(double filesize) throws ArithmeticException { 
+		if(filesize>freespace) {
+			throw new ArithmeticException("Error");
+		}
+		else{ 
+			file.add(filesize);
+			freespace -= filesize;
+		}
+	}
+	public void deletefile(int x) {
+		file.remove(x);
+	}
+	public boolean isFull() {
+		if(freespace = 0) 
+		}
+		
+	}
 }
